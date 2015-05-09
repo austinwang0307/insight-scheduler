35cf65da7fcd80d528df55b6a042f2d76d2eff58
maxcuz <austinwang0307@gmail.com>
2015-05-09 23:31:58 +0800

    added make_graph

--- insight-scheduler/insight_scheduler.py
+++ insight-scheduler/insight_scheduler.py
@@ -17,13 +17,17 @@ https://blueprints.launchpad.net/nova/+spec/vm-ensembles
 """
 
 import subprocess
+from graph_tool.all import *
 
-class InsightScheduler():
+class InsightScheduler(object):
     """Scheduler which awares of VC members."""
 
-    def schedule_cluster():
+    def schedule_cluster(self, vcg, pmg):
     	"""Mapping of VC members and PMs"""
 
     	# To-do:
     	# inputs: VC graph, PMs graph
+    	
+    def make_graph(self, vertex, weighted_edge):
+    	"""Construct graph of the given vertice and weighted edeges."""
     	
