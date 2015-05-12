fc4a1be073eddb7d5f9f3bf079328db884013d63
maxcuz <austinwang0307@gmail.com>
2015-05-12 10:10:12 +0800

    change InsightScheduler class structure

--- insight-scheduler/insight_scheduler.py
+++ insight-scheduler/insight_scheduler.py
@@ -16,14 +16,36 @@ https://blueprints.launchpad.net/nova/+spec/vm-ensembles
 """
 
 import sys
+import os
 import getopt
 import subprocess
 import simplejson as json
 from graph_tool.all import *
+from novaclient.v1_1 import client
 #from pprint import pprint
 
 class InsightScheduler(object):
     """Scheduler which awares of VC members."""
+
+    def __init__(self):
+        """Initial the scheduler"""
+
+        creds = __nova_creds()
+        nova = client.Client(**creds)
+        self.hypervisors = len(nova.hypervisors.list())
+
+    def __nova_creds(self):
+        """Handles nova credentials"""
+
+        cred = {}
+        cred['username'] = os.environ['OS_USERNAME']
+        cred['api_key'] = os.environ['OS_PASSWORD']
+        cred['auth_url'] = os.environ['OS_AUTH_URL']
+        cred['project_id'] = os.environ['OS_TENANT_NAME']
+        return cred
+
+    def __build_pmg(self):
+        """Build physical machines graph using the self.hypervisors property"""
 
     def schedule_cluster(self, vcg, pmg):
     	"""Mapping of VC members and PMs"""
@@ -31,13 +53,16 @@ class InsightScheduler(object):
     	# To-do:
     	# inputs: VC graph, PMs graph
     	
-    def make_graph(self, vertex, weighted_edge):
-    	"""Construct graph of the given vertice and weighted edeges."""
+    def build_vcg(self, vertices, edges):
+    	"""Build virtual cluster graph of the given vertices and edeges."""
+
+        # vertices: list of vertices' ids, eg. [1, 2, 3]
+        # edges: list of tuples of vertices, eg. [(1,2), (1,3)]
     	
 if __name__ == '__main__':
     """main function, begins right here."""
 
-    cluster = ''
+    cluster = ""
 
     try:
         opts, args = getopt.getopt(sys.argv[1:], "hc:", ["help", "cluster="])
